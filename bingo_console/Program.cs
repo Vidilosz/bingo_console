383a1b18c70365017dfe466ef7e5a5c0dc798509
Molnár Bence <molnar.bence@petrik.hu>
2026-03-04 10:25:53 +0100

    beolvasás

--- bingo_console/Program.cs
+++ bingo_console/Program.cs
@@ -4,8 +4,27 @@
     {
         static void Main(string[] args)
         {
-            
-        }
+			int[,] szamok = new int[5, 5];
+			string[] sorok = File.ReadAllLines("Andi.txt");
+
+			for (int i = 0; i < 5; i++)
+			{
+				string[] darabok = sorok[i].Split(';');
+
+				for (int j = 0; j < 5; j++)
+				{
+					if (darabok[j] == "X")
+					{
+						szamok[i, j] = 0;  
+					}
+					else
+					{
+						szamok[i, j] = int.Parse(darabok[j]);
+					}
+				}
+			}
+
+		}
 
 	}
 }
