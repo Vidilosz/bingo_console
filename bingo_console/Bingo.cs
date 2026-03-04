c1049e53eb1a175906ff73f0617eaccfbf00db16
Molnár Bence <molnar.bence@petrik.hu>
2026-03-04 09:28:12 +0100

    elmentem szünetre

--- bingo_console/Bingo.cs
+++ bingo_console/Bingo.cs
@@ -69,5 +69,25 @@ namespace bingo_console
 
 			return atlo2;
 		}
+		public void KartyaKiir()
+		{
+			Console.WriteLine(Nev + " kártyája:");
+
+			for (int i = 0; i < 5; i++)
+			{
+				for (int j = 0; j < 5; j++)
+				{
+					if (i == 2 && j == 2)
+						Console.Write(" X ");
+					else if (jelolve[i, j])
+						Console.Write($"{kartya[i, j],3}");
+					else
+						Console.Write("  0");
+
+					Console.Write(" ");
+				}
+				Console.WriteLine();
+			}
+		}
 	}
 }
