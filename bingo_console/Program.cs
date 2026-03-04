cabe326dbe4be96dfbffc9772e07971d454a4524
Molnár Bence <molnar.bence@petrik.hu>
2026-03-04 08:25:20 +0100

    Add project files.

---
+++ bingo_console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace bingo_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
